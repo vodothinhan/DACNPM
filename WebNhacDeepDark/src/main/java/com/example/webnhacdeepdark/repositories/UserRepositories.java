4d4aba674f7af2aca104315e6474e7d055c6cb9c
Admin <18130162@st.hcmuaf.edu.vn>
2022-05-22 01:00:57 +0700

    update validator in form login otp register

--- WebNhacDeepDark/src/main/java/com/example/webnhacdeepdark/repositories/UserRepositories.java
+++ WebNhacDeepDark/src/main/java/com/example/webnhacdeepdark/repositories/UserRepositories.java
@@ -4,5 +4,6 @@ import com.example.webnhacdeepdark.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepositories extends JpaRepository<Users, Integer> {
-
+    public Users findFirstByEmail(String email) ;
+    public Users findFirstByEmailAndPassword(String email , String pass);
 }

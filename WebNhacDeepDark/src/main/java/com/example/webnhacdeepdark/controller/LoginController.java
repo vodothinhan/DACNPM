95def8b5d870d589e8d51c67c3c80eb9b42fa1d8
Admin <18130162@st.hcmuaf.edu.vn>
2022-06-01 14:04:29 +0700

    merger main

--- WebNhacDeepDark/src/main/java/com/example/webnhacdeepdark/controller/LoginController.java
+++ WebNhacDeepDark/src/main/java/com/example/webnhacdeepdark/controller/LoginController.java
@@ -32,6 +32,6 @@ public class LoginController {
             model.addAttribute("login",false);
             return new ModelAndView("login",model);
         }
-        return new ModelAndView("login", model);
+        return new ModelAndView("redirect:/main", model);
     }
 }
